08074f9526fa4413c3c15370b13c7baffe65967a
Dane Brown <daneb.pc@gmail.com>
2012-04-13 10:42:33 -0700

    ssss

--- application/views/helpers/LoggedInAs.php
+++ application/views/helpers/LoggedInAs.php
@@ -5,12 +5,12 @@ class Zend_View_Helper_LoggedInAs extends Zend_View_Helper_Abstract {
     public function loggedInAs() {
         $auth = Zend_Auth::getInstance();
         if ($auth->hasIdentity()) {
-            $edit = $this->view->url(array('controller' => 'user', 'action' => 'edit'));
+//            $edit = $this->view->url(array('controller' => 'user', 'action' => 'edit'));
 
             $username = $auth->getIdentity();
             $logoutUrl = $this->view->url(array('controller' => 'auth',
                 'action' => 'logout'), null, true);
-            return 'Welcome ' . $username . '.<br /> <a href="' . $logoutUrl . '">Logout</a>' . ' | <a href="' . $edit . '">Edit</a>';
+            return 'Welcome ' . $username . '.<br /> <a href="' . $logoutUrl . '">Logout</a>';
         }
 
         $request = Zend_Controller_Front::getInstance()->getRequest();
